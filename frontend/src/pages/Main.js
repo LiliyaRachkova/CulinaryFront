4a76a6543b18ecfccbe43b7c21880b92412cc429
Konstantin <Bondkr@mail.ru>
2021-04-28 10:13:06 +0300

    init commit

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+
+const Main = () => {
+    return (
+        <div className="Main-page">
+            <h1 className="Main-header">Food Mood</h1>
+        </div>
+    )
+}
+
+export default Main
